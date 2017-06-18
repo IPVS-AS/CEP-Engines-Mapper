89b9a4f7061fa694088bf56e6876d6ee6d42136b
Leonardo Nahra <leonardonahra@gmail.com>
2017-06-18 10:38:10 +0200

    Add Esper demo

--- src/esperimage/src/main/java/com/ipvs/cepbenchmarking/App.java
+++ src/esperimage/src/main/java/com/ipvs/cepbenchmarking/App.java
@@ -4,6 +4,13 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URI;
 
+import com.espertech.esper.client.Configuration;
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPServiceProviderManager;
+import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.UpdateListener;
+import com.espertech.esper.client.EventBean;
+
 public class App {
     private static String exitMessage = "";
 
@@ -15,12 +22,27 @@ public class App {
             e.printStackTrace();
         }
 
+        Configuration config = new Configuration();
+        config.addEventTypeAutoName("com.ipvs.cepbenchmarking");
+        final EPServiceProvider serviceProvider = EPServiceProviderManager.getDefaultProvider(config);
+
+        String expression = "select avg(temperature) as avg_val from TemperatureEvent.win:time_batch(5 sec)";
+        EPStatement statement = serviceProvider.getEPAdministrator().createEPL(expression);
+
+        statement.addListener(new UpdateListener() {
+            public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+                EventBean event = newEvents[0];
+                System.out.println("MONITOR average temperature: " + event.get("avg_val"));
+            }
+        });
+
         TemperatureEventGenerator temperatureGenerator = new TemperatureEventGenerator(
             new TemperatureEventGenerator.EventHandler() {
                 public void handleEvent(TemperatureEvent temperatureEvent) {
                     System.out.println(temperatureEvent.toString());
+                    serviceProvider.getEPRuntime().sendEvent(temperatureEvent);
                 }
             });
-        temperatureGenerator.start(10);
+        temperatureGenerator.start(100);
     }
 }
