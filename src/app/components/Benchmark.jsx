a1fae7381291af3a41ea812562672752e0e50adf
Leonardo Nahra <leonardonahra@gmail.com>
2017-09-04 20:19:07 +0200

    Add benchmark configuration fill form

--- src/app/components/Benchmark.jsx
+++ src/app/components/Benchmark.jsx
@@ -3,31 +3,113 @@ import { Link } from 'react-router-dom';
 
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import Subheader from 'material-ui/Subheader';
 import { List, ListItem } from 'material-ui/List';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import Subheader from 'material-ui/Subheader';
+import IconButton from 'material-ui/IconButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-
-class Topic extends React.Component {
-  render() {
-    return (
-      <ListItem disabled={true}>
-        <TextField floatingLabelText="Statement name" />
-        <TextField floatingLabelText="Statement query" />
-      </ListItem>
-    );
-  }
-}
 
 class Benchmark extends React.Component {
   constructor() {
     super();
-    this.state = { topics: [<Topic />] };
+    this.state = {
+      config: {
+        broker: 'tcp://10.0.14.106:1883',
+        endEventName: 'TemperatureEndEvent',
+        events: [
+          {
+            name: 'TemperatureEvent',
+            properties: [
+              {
+                name: 'temperature',
+                type: 'int'
+              }
+            ]
+          }
+        ],
+        statements: [
+          {
+            name: 'AverageTemperature',
+            query:
+              'select avg(temperature) from TemperatureEvent.win:time_batch(5 sec)'
+          }
+        ]
+      }
+    };
+
+    this.addStatement = this.addStatement.bind(this);
+    this.addEvent = this.addEvent.bind(this);
   }
 
-  addTopic() {
-    this.setState({
-      topics: this.state.topics.concat([<Topic />])
+  handleChange(name, event) {
+    const value = event.target.value;
+    this.setState(state => {
+      var newState = { ...state };
+      newState.config[name] = value;
+      return newState;
+    });
+  }
+
+  handleChangeList(listName, name, index, event) {
+    const value = event.target.value;
+    this.setState(state => {
+      var newState = { ...state };
+      newState.config[listName][index][name] = value;
+      return newState;
+    });
+  }
+
+  handleChangeEventPropertyName(eventIndex, propertyIndex, event) {
+    const value = event.target.value;
+    this.setState(state => {
+      var newState = { ...state };
+      newState.config.events[eventIndex].properties[propertyIndex].name = value;
+      return newState;
+    });
+  }
+
+  handleChangeEventPropertyType(
+    eventIndex,
+    propertyIndex,
+    event,
+    index,
+    value
+  ) {
+    this.setState(state => {
+      var newState = { ...state };
+      newState.config.events[eventIndex].properties[propertyIndex].type = value;
+      return newState;
+    });
+  }
+
+  addEvent() {
+    this.setState(state => {
+      var newState = { ...state };
+      newState.config.events.push({
+        name: '',
+        properties: [{ name: '', type: 'string' }]
+      });
+      return newState;
+    });
+  }
+
+  addEventProperty(index) {
+    this.setState(state => {
+      var newState = { ...state };
+      newState.config.events[index].properties.push({
+        name: '',
+        type: 'string'
+      });
+      return newState;
+    });
+  }
+
+  addStatement() {
+    this.setState(state => {
+      var newState = { ...state };
+      newState.config.statements.push({ name: '', query: '' });
+      return newState;
     });
   }
 
@@ -56,28 +138,107 @@ class Benchmark extends React.Component {
         <TextField
           floatingLabelText="MQTT broker"
           floatingLabelFixed={true}
-          defaultValue="tcp://10.0.14.106:1883"
+          value={this.state.config.broker}
+          onChange={this.handleChange.bind(this, 'broker')}
         />
         <TextField
           floatingLabelText="End benchmark topic name"
           floatingLabelFixed={true}
-          defaultValue="TemperatureEndEvent"
+          value={this.state.config.endEventName}
+          onChange={this.handleChange.bind(this, 'endEventName')}
         />
         <List>
           <Subheader>
-            {'Topics'}
+            {'Events'}
           </Subheader>
-          {this.state.topics}
+          {this.state.config.events.map((event, eventIndex) =>
+            <ListItem key={eventIndex} disabled={true}>
+              <TextField
+                floatingLabelText="Event name"
+                value={event.name}
+                onChange={this.handleChangeList.bind(
+                  this,
+                  'events',
+                  'name',
+                  eventIndex
+                )}
+              />
+              {event.properties.map((property, propertyIndex) =>
+                <ListItem key={propertyIndex} disabled={true}>
+                  <TextField
+                    floatingLabelText="Property name"
+                    value={property.name}
+                    onChange={this.handleChangeEventPropertyName.bind(
+                      this,
+                      eventIndex,
+                      propertyIndex
+                    )}
+                  />
+                  <SelectField
+                    floatingLabelText="Property type"
+                    value={property.type}
+                    onChange={this.handleChangeEventPropertyType.bind(
+                      this,
+                      eventIndex,
+                      propertyIndex
+                    )}
+                  >
+                    <MenuItem value={'string'} primaryText="String" />
+                    <MenuItem value={'int'} primaryText="Integer" />
+                    <MenuItem value={'long'} primaryText="Long" />
+                    <MenuItem value={'boolean'} primaryText="Boolean" />
+                    <MenuItem value={'double'} primaryText="Double" />
+                    <MenuItem value={'float'} primaryText="Float" />
+                    <MenuItem value={'short'} primaryText="Short" />
+                    <MenuItem value={'char'} primaryText="Char" />
+                    <MenuItem value={'byte'} primaryText="Byte" />
+                  </SelectField>
+                </ListItem>
+              )}
+              <ListItem
+                primaryText="Add new event property"
+                leftIcon={<ContentAdd />}
+                onClick={this.addEventProperty.bind(this, eventIndex)}
+              />
+            </ListItem>
+          )}
           <ListItem
-            primaryText="Add new topic"
+            primaryText="Add new event"
             leftIcon={<ContentAdd />}
-            onClick={this.addTopic}
+            onClick={this.addEvent}
           />
-        </List>
-        <List>
           <Subheader>
             {'Statements'}
           </Subheader>
+          {this.state.config.statements.map((statement, index) =>
+            <ListItem key={index} disabled={true}>
+              <TextField
+                floatingLabelText="Statement name"
+                value={statement.name}
+                onChange={this.handleChangeList.bind(
+                  this,
+                  'statements',
+                  'name',
+                  index
+                )}
+              />
+              <TextField
+                floatingLabelText="Statement query"
+                value={statement.query}
+                onChange={this.handleChangeList.bind(
+                  this,
+                  'statements',
+                  'query',
+                  index
+                )}
+              />
+            </ListItem>
+          )}
+          <ListItem
+            primaryText="Add new statement"
+            leftIcon={<ContentAdd />}
+            onClick={this.addStatement}
+          />
         </List>
       </Paper>
     );
